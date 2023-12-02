af0ed0404d941cecfcb4f02308aa531b68e95c16
Saif Aljashamy <supersaifaljashamy@gmail.com>
2023-12-01 16:20:02 -0800

    Add 3d desktop to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,32 +20,20 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Software developer with 5+ years experience <br className='sm:block hidden' />
+          </p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Cal Poly Pomona graduate <br className='sm:block hidden' />
+          </p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Minor in Computer Science (4.00 GPA) <br className='sm:block hidden' />
+          </p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Major in Pure Mathetics
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={1} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -21,9 +21,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.35 : 0.7}
+        position={isMobile ? [0, 0.5, -1.2] : [0, -1.65, -1.5]}
+        rotation={[0, 0, -0.1]}
       />
     </mesh>
   );
